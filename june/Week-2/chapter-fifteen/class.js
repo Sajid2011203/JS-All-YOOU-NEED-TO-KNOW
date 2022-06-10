9d822a8e9552b632422a8399eb73263e95120053
maliha <maliha@gmail.com>
2022-06-10 20:02:51 +0600

    some update

---
+++ june/Week-2/chapter-fifteen/class.js
@@ -0,0 +1,47 @@
+// class kewyord
+'use strict'
+
+// class Person  {
+//     constructor(name, age) {
+//         this.name = name
+//         this.age= age
+//     }
+
+//     playing() {
+//         console.log(`${this.name} is playing`)
+//     }
+// }
+
+
+// let sakib = new Person('habib', 18)
+
+// console.log(sakib)
+
+
+// function Shape(width, height) {
+//     this.width = width
+//     this.height = height
+
+//     this.draw = function(){
+//         console.log(this)
+//     }
+// }
+
+
+// let Shape1 = new Shape(50, 45)
+
+// let test = Shape1.draw
+
+// test()
+
+
+// let obj = {
+//     draw: function () {
+//         console.log(this)
+//     }
+// }
+
+
+// let test = obj.draw
+
+// test()
